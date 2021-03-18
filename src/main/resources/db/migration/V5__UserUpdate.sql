a6e7bca68e2ba2a711eacfb06e2f4b7a2c4a4c17
ValdPiek <vald.piek@gmail.com>
2021-03-19 00:45:10 +0200

    Update V5__UserUpdate.sql

--- src/main/resources/db/migration/V5__UserUpdate.sql
+++ src/main/resources/db/migration/V5__UserUpdate.sql
@@ -1,3 +1,7 @@
-
 ALTER TABLE tbl_users_credentials
 ADD Last_Active TIMESTAMP NULL;
+
+ALTER VIEW viw_users_credentials
+AS
+    SELECT login,password,is_expired,Last_Active
+    FROM tbl_users_credentials;
