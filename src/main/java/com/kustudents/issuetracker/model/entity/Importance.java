b27fef1791374fb8fd03d90087a50a7079c6bdfd
Lukas <lukasjankauskasthelj@gmail.com>
2021-03-21 16:28:44 +0200

    Updated Importance mappinsg

--- src/main/java/com/kustudents/issuetracker/model/entity/Importance.java
+++ src/main/java/com/kustudents/issuetracker/model/entity/Importance.java
@@ -8,13 +8,14 @@ import lombok.Data;
 @Data
 @Entity(name = "viw_Importances")
 public class Importance {
- 
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
     @Column(name = "sort_order")
+    @OrderColumn
     private Long sortOrder;
 
     @NotBlank(message = "All Importances need to be named")
