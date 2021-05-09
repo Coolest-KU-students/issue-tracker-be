7e190b066b0b991b964cf56f8e5f10b14d3b85bf
ValdPiek <vald.piek@gmail.com>
2021-05-09 17:52:36 +0300

    Update UserReadRepository.java

--- src/main/java/com/kustudents/issuetracker/repository/UserReadRepository.java
+++ src/main/java/com/kustudents/issuetracker/repository/UserReadRepository.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 @Repository
 public interface UserReadRepository extends JpaRepository<UserRead, Long> {
 
